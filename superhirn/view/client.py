1b0637705c08129ccdd9b7e6786534ea4e43fbe5
tgiesa <till.giesa@outlook.com>
2023-12-09 15:17:17 +0100

    Pair-Programming with Marc Sabrautzki. changed parameter for 'prompt_for_code'

--- superhirn/view/client.py
+++ superhirn/view/client.py
@@ -3,11 +3,6 @@ import re
 
 class Client:
 
-    def __init__(self):
-        self.code_array = []
-        self.code_length = None
-        self.color_amount = None
-
     def prompt_for_role(self) -> str:
         """
         Prompts the user to select a role.
@@ -89,22 +84,23 @@ class Client:
             except ValueError:
                 print("Ungültige Eingabe. Bitte wählen Sie eine Zahl zwischen 2 und 8.")
 
-    def prompt_for_code(self) -> list:
+    def prompt_for_code(self, code_length, color_amount) -> list:
         """
         Prompts the user to set the code.
 
         :return: selected code.
         """
-        for i in range(self.code_length):
+        code_array = []
+        for i in range(code_length):
             while True:
                 try:
-                    element = int(input(f"Farbe an Stelle {i + 1} von {self.code_length}: "))
-                    if 1 <= element <= self.color_amount:
-                        self.code_array.append(element)
+                    element = int(input(f"Farbe an Stelle {i + 1} von {code_length}: "))
+                    if 1 <= element <= color_amount:
+                        code_array.append(element)
                         break
                     else:
-                        print(f"Ungültige Eingabe. Bitte wählen Sie eine Zahl zwischen 1 und {self.color_amount}.")
+                        print(f"Ungültige Eingabe. Bitte wählen Sie eine Zahl zwischen 1 und {color_amount}.")
                 except ValueError:
-                    print(f"Ungültige Eingabe. Bitte wählen Sie eine Zahl zwischen 1 und {self.color_amount}.")
-        print(f"Der Code lautet: {self.code_array}")
-        return self.code_array
+                    print(f"Ungültige Eingabe. Bitte wählen Sie eine Zahl zwischen 1 und {color_amount}.")
+        print(f"Der Code lautet: {code_array}")
+        return code_array
