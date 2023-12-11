468bbc5a4b257f8c852c31960d9e969a16b60c05
Louis Beul <louis@mbpr0.local>
2023-12-11 16:52:30 +0100

    (Philipp & Louis): Implement the first few methods against the test spec

--- superhirn/logic/encoder/local_encoder.py
+++ superhirn/logic/encoder/local_encoder.py
@@ -1,14 +1,20 @@
+import random
 from superhirn.logic.encoder.encoder_interface import EncoderInterface
+from superhirn.data.color import Color
+from superhirn.data.code import Code
 
 
 class LocalEncoder(EncoderInterface):
-    def __init__(self, code_length, available_colors):
+    def __init__(self, code_length: int, available_colors: list[Color]):
         self._code_length = code_length
         self._available_colors = available_colors
         self._generated_code = None
 
-    def generate_code(self):
-        pass
+    def generate_code(self) -> Code:
+        random_colors = [random.choice(self._available_colors) for __ in range(self._code_length)]
+        self._generated_code = Code(random_colors)
+        return self._generated_code
 
-    def rate(self, code_guess):
+    def rate(self, code_guess) -> Code:
+        rating: Code
         pass
--- superhirn/logic/gamecontroller/game_controller.py
+++ superhirn/logic/gamecontroller/game_controller.py
@@ -5,17 +5,20 @@ from superhirn.data.color import Color
 from superhirn.logic.decoder.decoder_interface import DecoderInterface
 from superhirn.logic.decoder.local_decoder import LocalDecoder
 from superhirn.logic.encoder.encoder_interface import EncoderInterface
-from superhirn.logic.gamecontroller.game_controller import GameControllerInterface
+from superhirn.logic.encoder.local_encoder import LocalEncoder
+from superhirn.logic.encoder.network_encoder import NetworkEncoder
+from superhirn.logic.gamecontroller.game_controller_interface import GameControllerInterface
 from superhirn.interfaces.data_interface import DataInterface
 
 
 def color_string_to_list(color_string: str) -> list[Color]:
-    split_values: list[str] = color_string.split(", ")
-    return [Color(val) for val in split_values]
+    split_values: list[str] = color_string.split(",")
+    return [Color(int(val)) for val in split_values]
 
 
 class GameController(GameControllerInterface):
     def __init__(self):
+        self._setup_completed = False
         self._current_game_statistics: DataInterface = DataInterface()
         self._encoder: Optional[EncoderInterface] = None
         self._decoder: Optional[DecoderInterface] = None
@@ -24,6 +27,21 @@ class GameController(GameControllerInterface):
         # TODO: Figure out how to create singleton in Python
         pass
 
-    def set_game_parameters(self, human_role: str, code_length: int, available_colors: str, is_online: bool) -> None:
+    def set_game_parameters(self, human_role: str, code_length: int, available_colors: str, is_online: bool,
+                            ip_address: str = None, port: int = None):
+        if self._setup_completed:
+            raise Exception("Error: Game was already set up!")
+
+        valid_colors: list[Color] = color_string_to_list(available_colors)
+
         if human_role.lower() == "codierer":
-            self._decoder = LocalDecoder(code_length, )
+            self._decoder = LocalDecoder(code_length, valid_colors)
+        elif human_role.lower() == "rater":
+            if not is_online:
+                self._encoder = LocalEncoder(code_length, valid_colors)
+            else:
+                self._encoder = NetworkEncoder(code_length, valid_colors, ip_address, port)
+        else:
+            raise Exception("Please choose either 'Rater' or 'Codierer' as role")
+
+        self._setup_completed = True
