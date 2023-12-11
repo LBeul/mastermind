35f9e9ccd53477ebeb0e20771a78f0ac86ef7dd8
Ole Gleß <s0564153@htw-berlin.de>
2023-12-11 22:40:05 +0000

    Update file test_data_modul.py

--- test/test_data_modul.py
+++ test/test_data_modul.py
@@ -2,6 +2,8 @@ import unittest
 
 from superhirn.data.code import Code
 from superhirn.data.color import Color
+from superhirn.data.game import Game
+from superhirn.data.rating import Rating
 
 
 class TestSimple(unittest.TestCase):
@@ -10,4 +12,20 @@ class TestSimple(unittest.TestCase):
         code = Code([Color.RED])
         self.assertEqual(code.get_color_at(0), Color.RED)
 
-# if __name__ == '__main__':
+    def test_add_question(self):
+        game = Game()
+        game.add_question(given_question=Code(colors=[Color.RED, Color.WHITE, Color.BLACK, Color.BLUE,
+                                                      Color.RED, Color.WHITE, Color.BLACK, Color.BLUE]))
+        self.assertIsNotNone(game.questions)
+
+    def test_add_rating(self):
+        game = Game()
+        rating = Rating(([Color.WHITE, Color.BLACK, Color.WHITE, Color.BLACK, Color.WHITE]))
+        game.add_rating(given_rating=rating)
+        self.assertIsNotNone(game.ratings)
+
+ #  def test_count_white(self):
+ #     code = [Color.WHITE, Color.BLACK, Color.WHITE, Color.BLACK, Color.WHITE]
+ #     rating = Rating(code)
+ #    count = rating.count_white()
+ #   self.assertEqual(3, count)
