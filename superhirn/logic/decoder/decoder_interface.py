44b31692182016136de1f42cc54a788b70fde745
Louis Beul <louis@mbpr0.local>
2023-12-11 16:50:10 +0100

    (Philipp & Louis): Update the exisiting interfaces to prepare for testing

--- superhirn/logic/decoder/decoder_interface.py
+++ superhirn/logic/decoder/decoder_interface.py
@@ -1,9 +1,11 @@
 from abc import ABC, abstractmethod
 
+from superhirn.data.code import Code
+
 
 class DecoderInterface(ABC):
     @abstractmethod
-    def guess(self):
+    def guess(self) -> Code:
         """
         Generates a code guess of the given length, using the available colors
         :return: the newly generated Code instance
--- superhirn/logic/gamecontroller/game_controller_interface.py
+++ superhirn/logic/gamecontroller/game_controller_interface.py
@@ -11,12 +11,16 @@ class GameControllerInterface(ABC):
         pass
 
     @abstractmethod
-    def set_game_parameters(self, human_role: str, code_length: int, available_colors: str, is_online: bool) -> None:
+    def set_game_parameters(self, human_role: str, code_length: int, available_colors: str, is_online: bool,
+                            ip_address: str = None, port: int = None):
         """
         Sets the initial parameters for the Game to be started
         :param human_role: the role taken by the user
         :param code_length: the fixed code length of the game
         :param available_colors: the colors available to compose the Code of
         :param is_online: indicates whether the game shall take place locally or against a remote server encoder
+        :param ip_address: IP address of the remote encoder
+        :param port: network port of the remote encoder
+
         """
         pass
