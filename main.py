ceb226acf38c0dc82b98f632803690cac9627e93
tgiesa <till.giesa@outlook.com>
2023-12-12 09:51:28 +0100

    (Marc & Till): changed location auf main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from superhirn.view.client import Client
-from superhirn.view.main_menu_view import MainMenuView
 
 if __name__ == '__main__':
     client_instance = Client()
-    client_instance.start_game()
-
+    client_instance.command_loop()
