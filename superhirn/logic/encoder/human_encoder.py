c323d767b20e8cd88f102c56360a382c9b6a652c
tgiesa <till.giesa@outlook.com>
2023-12-21 22:38:44 +0100

    (Marc & Till): added HumanDecoder and HumanEncoder

---
+++ superhirn/logic/encoder/human_encoder.py
@@ -0,0 +1,21 @@
+from superhirn.data.code import Code
+from superhirn.data.rating import Rating
+from superhirn.logic import color_string_to_list
+from superhirn.logic.encoder.encoder_interface import EncoderInterface
+from superhirn.logic.ui_connector.ui_connector_interface import UiControllerInterface
+
+
+class HumanEncoder(EncoderInterface):
+    def __init__(self, ui: UiControllerInterface):
+        self._ui = ui
+        self._generated_code = None
+
+    def generate_code(self, code_length: int, color_availabilities: int) -> Code:
+        input_string = self._ui.prompt_for_code(code_length, color_availabilities)
+        color_list = color_string_to_list(input_string)
+        return Code(color_list)
+
+    def rate(self, code_guess: Code) -> Rating:
+        input_string = self._ui.prompt_for_rating(len(code_guess.to_int_list()))
+        color_list = color_string_to_list(input_string)
+        return Rating(color_list)
